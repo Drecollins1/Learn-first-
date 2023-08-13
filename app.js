b1f9aa33371a15ff27a045a7d2a4a0367f4b5beb
Drecollins1 <115148885+Drecollins1@users.noreply.github.com>
2023-08-13 23:13:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// const getPost = async() => {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     const data = await response.json();
+//     console.log(data)
+// }
+// getPost();
+
+var person = {
+    name: "John", age: 31, 
+    favColor: "green", height: 183
+   };
